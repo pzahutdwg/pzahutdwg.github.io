3a1ea605cfeb6dd6a4eef28d95b1174b394f69ed
Truit Elwell <145386841+pzahutdwg@users.noreply.github.com>
2026-04-16 10:27:10 -0400

    agadg

--- js/switchPages.js
+++ js/switchPages.js
@@ -1,12 +1,12 @@
 const page = document.getElementById("content");
 setTimeout(() => {
     page.style.opacity = '1';
-}, 1000);
+}, 1);
 
 function switchToPage(pageName) {
     page.style.opacity = '0';
     // Wait before switching pages
     setTimeout(() => {
         window.location.href = pageName;
-    }, 1000);
+    }, 250);
 }
